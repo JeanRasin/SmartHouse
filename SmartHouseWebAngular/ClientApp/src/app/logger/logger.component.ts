c991da11473c52212d000fd7e78bf951cb10e501
jean07011989 <logun2@yandex.ru>
2020-01-29 13:13:15 +0500

    Clear code 1

--- SmartHouseWebAngular/ClientApp/src/app/logger/logger.component.ts
+++ SmartHouseWebAngular/ClientApp/src/app/logger/logger.component.ts
@@ -1,30 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-//import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 
 import { Logger } from 'src/app/logger';
 import { HttpService } from 'src/app/http.service';
-
-//export interface PeriodicElement {
-//  name: string;
-//  logLevel: number;
-//  eventId: number;
-//  message: string;
-//}
-
-//const ELEMENT_DATA: PeriodicElement[] = [
-//  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-//  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-//  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-//  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-//  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-//  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-//  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-//  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-//  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-//  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-//];
 
 @Component({
   selector: 'logger-component',
@@ -38,7 +17,6 @@ export class LoggerComponent implements OnInit {
   constructor(private httpService: HttpService) { }
 
   displayedColumns: string[] = ['date', 'logLevel', 'eventId', 'message'];
-  // data: Logger[];
   dataSource: MatTableDataSource<Logger>;
 
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
@@ -48,10 +26,7 @@ export class LoggerComponent implements OnInit {
     this.httpService.getLogger().subscribe((data: Logger[]) => {
 
       data.sort(function (a, b) {
-        // Turn your strings into dates, and then subtract them
-        // to get a value that is either negative, positive, or zero.
-       return new Date(b.date).getTime() - new Date(a.date).getTime();//Math.abs()
-        // return new Date(b.Date) - new Date(a.Date);
+        return new Date(b.date).getTime() - new Date(a.date).getTime();
       });
 
       console.log(data);
