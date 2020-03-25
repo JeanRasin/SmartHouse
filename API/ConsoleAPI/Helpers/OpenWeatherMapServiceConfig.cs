61ecca7501706ec82af22fba48b9b6be644764cd
jean07011989 <logun2@yandex.ru>
2020-03-25 11:42:01 +0500

    Clear ConsoleAPI

--- API/ConsoleAPI/Helpers/OpenWeatherMapServiceConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConsoleAPI.Helpers
-{
-    public class OpenWeatherMapServiceConfig
-    {
-        public string Url { get; set; }
-        public string City { get; set; }
-        public string Api { get; set; }
-    }
-}
